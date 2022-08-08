97e11d41efd1f6f120f16f4f6d5e5f6e60c299bf
JonathanKarni165 <77381692+JonathanKarni165@users.noreply.github.com>
2022-08-08 19:38:02 +0300

    Add files via upload

---
+++ C# script/Animation Scripts/ButtonAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using TMPro;
+
+public class ButtonAnimation : MonoBehaviour
+{
+    public TextMeshProUGUI tm;
+    public GameObject loadingSym;
+    public Animator controller;    
+    private bool isPressed = false;
+
+    public void Pressed()
+    {
+        isPressed = !isPressed;
+        controller.SetBool("Pressed", isPressed);
+        tm.text = isPressed ? "Populating" : "Quick Match";
+        loadingSym.SetActive(isPressed);
+    }
+}
---
+++ C# script/Animation Scripts/CustumPropetySetup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+
+public class CustumPropetySetup : MonoBehaviourPunCallbacks
+{
+    private ExitGames.Client.Photon.Hashtable myCustomProperties = new ExitGames.Client.Photon.Hashtable();
+
+    private int ActorNumber;
+    private bool isWaitingRoomSceneActive;
+
+    void Start()
+    {
+        isWaitingRoomSceneActive = SceneManager.GetActiveScene().buildIndex == 1;
+        if (isWaitingRoomSceneActive)
+            SetCustomProperty();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        SetCustomProperty();
+    }
+
+    public void SetCustomProperty()
+    {
+        for(int i=0; i<PhotonNetwork.PlayerList.Length; i++)
+        {
+            if(PhotonNetwork.PlayerList[i].IsLocal)
+            {
+                myCustomProperties["ActorNumber"] = i;
+                myCustomProperties["HandColor"] = PlayerPrefs.GetInt("HandColor");
+                myCustomProperties["Skin"] = PlayerPrefs.GetInt("Skin");
+                myCustomProperties["SkinColor"] = PlayerPrefs.GetInt("SkinColor");
+                myCustomProperties["IsMaskGraphic"] = PlayerPrefs.GetInt("IsMaskGraphic");
+                PhotonNetwork.SetPlayerCustomProperties(myCustomProperties);
+                ActorNumber = i;
+            }
+        }
+    }
+}
